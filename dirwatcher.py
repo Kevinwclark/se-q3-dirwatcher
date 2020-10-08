8868e1392af7bca4dbb1ca7affb58eef5a73d135
Kevin Clark <kevinwilliamclark@gmail.com>
2020-10-08 09:04:07 -0500

    continuing the framework of this project.

--- dirwatcher.py
+++ dirwatcher.py
@@ -7,6 +7,8 @@ __author__ = "Kevin Clark"
 
 import sys
 import argparse
+import logger
+import signal
 
 
 def search_for_magic(filename, start_line, magic_string):
@@ -14,19 +16,43 @@ def search_for_magic(filename, start_line, magic_string):
     return
 
 
+def scan_single_file():
+    pass
+
+
+def detect_added_files():
+    pass
+
+
+def detect_removed_files():
+    pass
+
+
 def watch_directory(path, magic_string, extension, interval):
-    # Your code here
+    # os.listdir() is a good way to see the dirs.
+    # os.path.splitext(path) can be used to check for the extension
     return
 
 
 def create_parser():
     parser = argparse.ArgumentParser(
         description="Watches specified directory.")
+    # what dir to watch, magic string, extension and interval
     return parser
 
 
 def signal_handler(sig_num, frame):
-    # Your code here
+    """
+    This is a handler for SIGTERM and SIGINT. Other signals
+    can be mapped here as well (SIGHUP?)
+    Basically, it just sets a global flag, and main() will
+    exit its loop if the signal is trapped.
+    :param sig_num: The integer signal number that was trapped from the OS.
+    :param frame: Not used
+    :return None
+    """
+    # log the associated signal name
+    logger.warn('Received ' + signal.Signals(sig_num).name)
     return
 
 
