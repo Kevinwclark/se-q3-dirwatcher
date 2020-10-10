f73068df872a833ad1b7d056397fcf7481d245aa
Kevin Clark <kevinwilliamclark@gmail.com>
2020-10-09 20:39:24 -0500

    watch_directory() function can now watch a dir and update a dictionary with files

--- dirwatcher.py
+++ dirwatcher.py
@@ -7,31 +7,26 @@ __author__ = "Kevin Clark with help from JT and Joseph Hafed"
 
 import sys
 import argparse
-# import logger
+import logging
 import signal
+import time
+import os
 
 
-dictionary = {} # keys will be filenames and values will be last line read
-# exit flag
+exit_flag = False
 
 
-def search_for_magic(filename, start_line, magic_string):
-    line_number = 0
-    with open(filename) as f:
+def search_for_magic(path, filename, start_line, magic_string):
+    line_number = start_line
+    with open(f'{path}/{filename}') as f:
         lines = f.readlines()
     num_of_lines = len(lines)
     for i in range(start_line, num_of_lines):
         if magic_string in lines[i]:
-            line_number = i + 1
+            print(f'word found in {filename} {i + 1}!')
+            # logging moment here
+        line_number = i + 1
     return line_number
-
-
-def scan_single_file():
-    pass
-
-
-def detect_added_files():
-    pass
 
 
 def detect_removed_files():
@@ -39,7 +34,31 @@ def detect_removed_files():
 
 
 def watch_directory(path, magic_string, extension, interval):
-    return
+    dictionary = {}
+    while not exit_flag:
+        time.sleep(interval)
+        if os.path.isdir(path):
+            file_list = os.listdir(path)
+            for files in file_list:
+                if files.endswith(extension) and files not in dictionary:
+                    dictionary[files] = 0
+                    print(f'file added {files}')
+                    # logging moment here
+            key = list(dictionary.keys())
+            for k in key:
+                if k not in file_list:
+                    print(f'this file was removed {k}')
+                    dictionary.pop(k)
+                    # logging moment here
+            for key, value in dictionary.items():
+                dictionary[key] = search_for_magic(path, key, value, magic_string)
+            print(dictionary)
+
+        else:
+            print("this path does not exist")
+            dictionary = {}
+        
+    pass
 
 
 def create_parser():
@@ -58,29 +77,50 @@ def signal_handler(sig_num, frame):
     """
     This is a handler for SIGTERM and SIGINT. Other signals
     can be mapped here as well (SIGHUP?)
-    Basically, it just sets a global flag, and main() will              
+    Basically, it just sets a global flag, and main() will            
     exit its loop if the signal is trapped.
     :param sig_num: The integer signal number that was trapped from the OS.
     :param frame: Not used
     :return None
     """
-    # log the associated signal name
-    # logger.warn('Received ' + signal.Signals(sig_num).name)
+    global exit_flag
+    exit_flag = True
+    logging.warning('Received ' + signal.Signals(sig_num).name)
     return
 
 
 def main(args):
-    """Implementation of dirwatcher"""
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
     parser = create_parser()
     ns = parser.parse_args(args)
-    print(ns)
 
     if not ns:
         parser.print_usage(args)
         sys.exit(1)
+
+    path = ns.directory
+    magic = ns.magic
+    ext = ns.extension
+    integer = ns.integer
+
+    while not exit_flag:
+        # start_time = time.time()
+        # stop_time when signal is received to stop running. 
+        # then log the start_time - stop_time
+        try:
+            print('something')
+            watch_directory(path, magic, ext, integer)
+        except FileNotFoundError as err:
+            print('Sorry, this file does not exist')
+            logging.error(err)
+      
+    # final exit point happens here
+    # Log a message that we are shutting down
+    # Include the overall uptime since program start
     return
 
 
 if __name__ == '__main__':
     main(sys.argv[1:])
-    print(sys.argv[1:])
